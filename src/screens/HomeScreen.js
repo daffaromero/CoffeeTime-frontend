056e7a7ec4f0330519a382e475d84e4966b75122
daffaromero <daffaromero@mail.ugm.ac.id>
2022-11-16 14:01:13 +0700

    starting axios

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
-import Product from "../components/Product";
-import products from "../products";
+import axios from "axios";
+import Menu from "../components/Menu";
 
 const HomeScreen = () => {
+  const [menus, setMenus] = useState([]);
+  useEffect(() => {
+    const fetchMenus = async () => {
+      const res = await axios.get("/api/v1/menu");
+
+      setMenus(res);
+      {
+        console.log(res.data);
+        let stringData = JSON.stringify(res.data);
+        let menuData = JSON.parse(stringData);
+        console.log(menuData.data[0].name);
+      }
+    };
+    fetchMenus();
+  }, []);
   return (
     <>
       <h1>Menu</h1>
       <Row>
-        {products.map((product) => (
+        {Object.keys(menus).map((key) => {
           <Col sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
+            <Menu menu={menus} />
+          </Col>;
+        })}
       </Row>
     </>
   );
