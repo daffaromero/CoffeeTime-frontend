b6ab6630f5f0a9b317ce638badfb0c7cbfebe289
daffaromero <daffaromero@mail.ugm.ac.id>
2022-11-16 20:51:52 +0700

    Version controlling

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,21 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
 
-const Product = ({ prod }) => {
+const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-      <Link to={`/product/${prod._id}`}>
-        <Card.Img src={prod.image} variant='top' />
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
       </Link>
 
       <Card.Body>
-        <Link to={`/product/${prod._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as='div'>
-            <strong>{prod.name}</strong>
+            <strong>{product.name}</strong>
           </Card.Title>
         </Link>
 
-        <Card.Text as='h3'>${prod.price}</Card.Text>
+        <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,6 @@ import { Row, Col } from "react-bootstrap";
 import axios from "axios";
 import Product from "../components/Product";
 import drinks from "../products";
-import drinks2 from "../products2";
 
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
@@ -24,31 +23,11 @@ const HomeScreen = () => {
     fetchMenus();
   }, []);
   console.log("State before render: ", menus);
-  // for (var i = 0; i < menus.length; i++) {
-  //   var MenuCard = ({ menu }) => {
-  //     <Card className='my-3 p-3 rounded'>
-  //       <Link to={`/product/${menu._id}`}>
-  //         <Card.Img src={menu.image} variant='top' />
-  //       </Link>
-  //       <Card.Body>
-  //         <Link to={`/product/${menu._id}`}>
-  //           <Card.Title as='div'>
-  //             <strong>{menu.name}</strong>
-  //           </Card.Title>
-  //         </Link>
-  //         <Card.Text as='h3'>${menu.price}</Card.Text>
-  //       </Card.Body>
-  //     </Card>;
-  //   };
-  //   console.log("Menus i", menus[i]);
-  let drunk = drinks2[0].data;
-  console.log(drunk);
-  console.log(drinks);
   return (
     <>
       <h1>Menu</h1>
       <Row>
-        {drunk.map((prod) => (
+        {menus.map((prod) => (
           <Col key={prod._id} sm={12} md={6} lg={4} xl={3}>
             <Product product={prod} />
           </Col>
