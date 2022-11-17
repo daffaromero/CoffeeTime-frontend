dcc75efb8d9c802a4a8c0e49202df6e7f9a06ae0
nrwfebriani <nur.wulan.febriani@mail.ugm.ac.id>
2022-11-17 23:05:11 +0700

    add : cart

--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,11 +1,12 @@
 import React, {useEffect} from 'react'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams, useLocation} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import {Row, Col, ListGroup, Image, Form, Button, Card} from 'react-bootstrap'
 import Message from '../components/Message'
 import { addToCart } from '../actions/cartActions'
 
-const CartScreen = ({props, location, history}) => {
+const CartScreen = ({props, history}) => {
+    let location = useLocation();
     const { id } = useParams();
     const productId = id;
 
