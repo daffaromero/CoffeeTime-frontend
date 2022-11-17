1058429203369480811f2c9fdb10e2aec4024d9b
nurazzamwafi <112134253+Nur-Azzam-Wafiuddin@users.noreply.github.com>
2022-11-17 22:31:23 +0700

    Add User Sign In Screen
    Still need to be fix, required backend Functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} />
+            <Route path='/login' element={<LoginScreen/>} />
             <Route path='/product/:id' element={<ProductScreen />} />
           </Routes>
         </Container>
