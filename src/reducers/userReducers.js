60237d3de38571c1ae7f9a03b90c80cdd9f71997
nurazzamwafi <112134253+Nur-Azzam-Wafiuddin@users.noreply.github.com>
2022-11-17 21:12:28 +0700

    Add User Login Reducer and Action

---
+++ src/reducers/userReducers.js
@@ -0,0 +1,19 @@
+import {
+    USER_LOGIN_REQUEST,
+    USER_LOGIN_SUCCESS,
+    USER_LOGIN_FAIL,
+    USER_LOGOUT
+} from '../constants/userConstants'
+
+export const userLoginReducer = (state = {}, action) => {
+    switch (action.type) {
+      case USER_LOGIN_REQUEST:
+        return { loading: true, menus: [] };
+      case USER_LOGIN_SUCCESS:
+        return { loading: false, menus: action.payload };
+      case USER_LOGIN_FAIL:
+        return { loading: false, error: action.payload };
+      default:
+        return state;
+    }
+};
--- src/store.js
+++ src/store.js
@@ -3,13 +3,22 @@ import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { menuListReducer, menuDetailsReducer } from "./reducers/menuReducers";
+import { userLoginReducer } from "./reducers/userReducers"
 
 const reducer = combineReducers({
   productList: menuListReducer,
   productDetails: menuDetailsReducer,
+  userLogin: userLoginReducer
 });
 
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null
+
+const initialState = {
+
+  userLogin: {userInfo: userInfoFromStorage}
+};
 
 const middleware = [thunk];
 
