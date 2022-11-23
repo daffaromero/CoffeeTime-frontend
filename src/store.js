4988c3c7acb9c0bf10226543369ce9ef689ed0c7
nrwfebriani <nur.wulan.febriani@mail.ugm.ac.id>
2022-11-23 20:19:22 +0700

    Adding ProfileScreen to update user profile

--- src/store.js
+++ src/store.js
@@ -3,13 +3,14 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { menuListReducer, menuDetailsReducer } from "./reducers/menuReducers";
 import { cartReducer } from "./reducers/cartReducers";
-import { userLoginReducer, userRegisterReducer } from "./reducers/userReducers";
+import { userLoginReducer, userRegisterReducer, userDetailsReducer } from "./reducers/userReducers";
 
 const reducer = combineReducers({
   productList: menuListReducer,
   productDetails: menuDetailsReducer,
   userLogin: userLoginReducer,
   userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
   cart: cartReducer,
 });
 
